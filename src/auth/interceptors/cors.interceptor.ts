557685cb3439ff67453e5b25559725e9c4ebd78e
luka3l22 <luka3l22.lee@gmail.com>
2023-02-16 18:07:49 -0800

    add more response headers

--- src/auth/interceptors/cors.interceptor.ts
+++ src/auth/interceptors/cors.interceptor.ts
@@ -9,6 +9,9 @@ export class CorsInterceptor implements NestInterceptor {
       map((data) => {
         const response = context.switchToHttp().getResponse();
         response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        response.header('Access-Control-Allow-Credentials', 'true');
         return data;
       }),
     );
