28ccd1a0b3235ea7c25a16c3b0719625c0387cfc
luka3l22 <luka3l22.lee@gmail.com>
2023-02-16 22:45:53 -0800

    app setup

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,61 @@
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as connectPgSimple from 'connect-pg-simple';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
-import { setup } from './setup';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: {
-    origin: '*', // Allow all origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allow all HTTP methods
-  }});
+  const app = await NestFactory.create(AppModule);
 
-  let appSetuped = setup(app);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
 
-  await appSetuped.listen(process.env.VALLENTUNA_SURVEY_BACKEND_PORT || 5000);
+  app.use(cookieParser(process.env.VALLENTUNA_SURVEY_BACKEND_APP_SECRET));
+
+  app.use(
+    session({
+      secret: process.env.VALLENTUNA_SURVEY_BACKEND_APP_SECRET as string,
+      resave: false,
+      saveUninitialized: false,
+      store:
+        process.env.VALLENTUNA_SURVEY_BACKEND_NODE_ENV === 'production'
+          ? new (connectPgSimple(session))()
+          : new session.MemoryStore(),
+      cookie: {
+        httpOnly: true,
+        signed: true,
+        sameSite: 'strict',
+        secure: process.env.VALLENTUNA_SURVEY_BACKEND_NODE_ENV === 'production',
+      },
+    }),
+  );
+
+
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  const options = new DocumentBuilder()
+    .setTitle('Vallentuna Survey Tool API Documentation')
+    .setDescription('This documentation is for Vallentuna Survey Tool')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+
+  await app.listen(process.env.VALLENTUNA_SURVEY_BACKEND_PORT || 5000);
 }
 bootstrap();
