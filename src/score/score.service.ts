cb804eae0f53495bcbc53f554c92b856ba456125
luka3l22 <luka3l22.lee@gmail.com>
2023-06-01 07:56:09 +0800

    fix getFollowUpFilterResult function

--- src/score/score.service.ts
+++ src/score/score.service.ts
@@ -58,12 +58,13 @@ export class ScoreService extends TypeOrmCrudService<ScoreEntity> {
 
   async numOfClients(codeNumbers: string[], startDate: string, endDate: string, occasions: OccasionIndex[]) {
     return (codeNumbers.length === 0 || occasions.length === 0) ? 0 : await this.repo.createQueryBuilder("score")
-      .select("COUNT(score.codeNumber)", "count")
+      .distinct(true)
+      .select("score.codeNumber")
       .where("score.codeNumber IN (:...codeNumbers)", { codeNumbers })
       .andWhere("score.occasion IN (:...occasionNumbers)", { occasionNumbers: occasions })
       .andWhere("score.date BETWEEN :startDate AND :endDate", { startDate, endDate })
       .groupBy("score.codeNumber")
-      .getRawOne();
+      .getCount();
   }
 
   async avgOfOrsAndScore15(codeNumbers: string[], occasions: OccasionIndex[]) {
@@ -300,7 +301,7 @@ export class ScoreService extends TypeOrmCrudService<ScoreEntity> {
     }
 
     // filter score table with codeNumbers and date ranges
-    const { count: numOfClients } = await this.numOfClients(codeNumbers, payload.startDate, payload.endDate, payload.occasions) || { count: 0 };
+    const numOfClients = await this.numOfClients(codeNumbers, payload.startDate, payload.endDate, payload.occasions);
     const numOfCodeNumbers = await this.backgroundMetadataService.count();
     const percentage = ((numOfCodeNumbers === 0) ? 0 : (+numOfClients / numOfCodeNumbers)) * 100;
     const { ors, score15 } = await this.avgOfOrsAndScore15(codeNumbers, payload.occasions);
