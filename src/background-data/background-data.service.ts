7875fd461f0326493a964690acab8be90c233493
luka3l22 <luka3l22.lee@gmail.com>
2023-10-05 19:38:33 +0900

    domain +uri

--- src/background-data/background-data.service.ts
+++ src/background-data/background-data.service.ts
@@ -492,7 +492,7 @@ export class BackgroundDataService {
       });
       doc.render({
         date: dayjs().format("YYYY-MM-DD"),
-        string: domain
+        string: domain + "/" + uri
         // QRCODE: domain + "/" + atob(atob(atob(uri)))
       });
       const buf = doc.getZip().generate({
