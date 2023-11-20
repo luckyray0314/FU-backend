eac05fe7f4fb7cc41e76c2548a47847eb6882d17
Yasser Vaz d'Almeida <yasilvalmeida@hotmail.com>
2023-11-20 13:43:18 +0000

    Add pm2 commands

--- deploy/deploy-production.sh
+++ deploy/deploy-production.sh
@@ -5,6 +5,9 @@ DEPLOY_SERVER=su.eagle.productions
 echo "Deploying to production into ${DEPLOY_SERVER}"
 ssh cooldev@${DEPLOY_SERVER} 'bash' < ./deploy/server.sh
 
+# Install last PM2
+yarn global add pm2
+
 # Pull code
 cd /home/cooldev/vallentuna/production
 git checkout main
@@ -13,4 +16,5 @@ git pull origin main
 # Build and deploy
 yarn install
 yarn run build
-pm2 restart server
+pm2 --name vallentuna-backend-production start dist/main.js
+pm2 restart vallentuna-backend-production
