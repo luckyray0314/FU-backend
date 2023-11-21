31bd4ab116093dd4e71749e9c76aeb7de36b2f6f
Yasser Vaz d'Almeida <yasilvalmeida@hotmail.com>
2023-11-21 19:22:49 +0000

    Add development env CI

--- deploy/development.sh
+++ deploy/development.sh
@@ -3,18 +3,27 @@ DEPLOY_USER=cooldev
 DEPLOY_SERVER=vallentuna.su.eagle.productions
 
 echo "Deploying to development into ${DEPLOY_SERVER}"
-ssh ${DEPLOY_USER}@${DEPLOY_SERVER} 'bash' < ./deploy/development.sh
-
-# Install last PM2
-yarn global add pm2
+ssh ${DEPLOY_USER}@${DEPLOY_SERVER} 'bash' < ./deploy/test.sh
 
 # Pull code
 cd /home/cooldev/vallentuna/development
-git checkout main
-git pull origin main
+git checkout development
+git pull origin development
+
+#NVM
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash 
+export NVM_DIR="$HOME/.nvm" && . "$NVM_DIR/nvm.sh" --no-use
+eval "[ -f .nvmrc ] && nvm install || nvm install v18"
+node --version
+npm --version
+sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
+sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
+
+# Node version
+node --version
 
 # Build and deploy
-yarn install
-yarn run build
-pm2 --name vallentuna-backend-development start dist/main.js
-pm2 restart vallentuna-backend-development
+npm ci
+npm install env-cmd
+npm run build
+pm2 start ../ecosystem-development.config.js
